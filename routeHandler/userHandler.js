4ee8b5c48c97276419ec24f92c0f28d661759990
Reazour Rahaman Rabbi <alexrabbi111@gmail.com>
2022-06-28 17:10:14 +0600

    quiz-marks-schema

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const { default: mongoose } = require('mongoose')
-const {registerUser, loginUser, forgotPassword, resetPassword, updateUser, getUserInfo, deleteUser, getSingleUserInfo, getAllUser, updateRole} = require('../controllers/userController')
+const {registerUser, loginUser, forgotPassword, resetPassword, updateUser, getUserInfo, deleteUser, getSingleUserInfo, getAllUser, updateRole,getAssignmentMarks} = require('../controllers/userController')
 const router=express.Router()
 const userSchema=require('../schemas/userSchema')
 const User= new mongoose.model('User',userSchema)
@@ -16,6 +16,7 @@ router.route('/forgotpassword').post(forgotPassword)
 router.route("/passwordreset/:resetToken").put(resetPassword);
 router.route("/teacher").post(addTeacher);
 router.route("/role").put(updateRole)
+router.route("/assignmentMarks").get(getAssignmentMarks)
 
 
 module.exports=router
