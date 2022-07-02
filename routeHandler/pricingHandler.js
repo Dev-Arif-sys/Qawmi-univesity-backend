ab9ee9584ceaa92d6e1845a47e0b110d6dfe406e
Taniatabassumnoor <taniatabassumnoor866@gmail.com>
2022-07-03 05:57:49 +0600

    digital ocean done

--- routeHandler/pricingHandler.js
+++ routeHandler/pricingHandler.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const {
-  insertPricing,
   getAllPricing,
   updatePricing,
   getSinglePricing,
@@ -10,9 +9,8 @@ const {
 const { checkLogin, admin } = require("../middlewares/checkLogin");
 
 // all routes
-router.post("/createPricing", insertPricing);
 router.get("/getPricing", getAllPricing);
 router.get("/getPricing/:pricingId", getSinglePricing);
-router.put("/getPricing/:pricingId", updatePricing);
+router.patch("/getPricing/:pricingId", updatePricing);
 
 module.exports = router;
